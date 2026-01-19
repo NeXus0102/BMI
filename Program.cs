f58d55330f89c0288392a075a862aeb051d7c48d
molnartm <molnartm@310-18>
2026-01-19 08:40:15 +0100

    Adatszerkezet Feltoltese

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@
         static void Main(string[] args)
         {
             List<Diak> list = new List<Diak>();
+            var sorok=File.ReadAllLines("BMI.txt", System.Text.Encoding.Latin1).Skip(1);
+            foreach (var sor in sorok)
+            {
+                //Console.WriteLine(sor);
+                string[] darabok = sor.Split(';');
+                string nev = darabok[0];
+                int eletkor = Convert.ToInt32(darabok[1]);
+                int magassag = Convert.ToInt32(darabok[2]);
+                int suly = Convert.ToInt32(darabok[3]);
+                Diak d=new Diak(nev,eletkor,magassag,suly);
+                list.Add(d);
+            }
         }
     }
 }
